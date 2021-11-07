88c8b1b258216026f953426c92c92bcce7320d40
sergio <me@sergiomartinrubio.com>
2021-11-07 18:07:22 +0100

    fix env variable name

--- coolmod_crawler/spiders/db.py
+++ coolmod_crawler/spiders/db.py
@@ -8,7 +8,7 @@ def get_sql_connector():
     global __connection
     if not __connection:
         __connection = mariadb.connect(
-            user=os.getenv("MARIADB_PASSWORD"),
+            user=os.getenv("MARIADB_USER"),
             password=os.getenv("MARIADB_PASSWORD"),
             host="localhost",
             port=3306,
