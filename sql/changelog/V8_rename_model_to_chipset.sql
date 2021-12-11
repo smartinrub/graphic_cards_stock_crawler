cb3b7e9dd8c5e6ad117ed44a8c02ef3912ce1277
sergio <me@sergiomartinrubio.com>
2021-12-11 23:41:39 +0100

    Fix changelog

--- sql/changelog/V8_rename_model_to_chipset.sql
+++ sql/changelog/V8_rename_model_to_chipset.sql
@@ -1,3 +1,3 @@
 USE graphic_cards_stock_crawler;
-ALTER TABLE graphic_card RENAME COLUMN model TO chipset;
-ALTER TABLE stock RENAME COLUMN model TO chipset;
+ALTER TABLE graphic_card CHANGE COLUMN model chipset VARCHAR(256);
+ALTER TABLE stock CHANGE COLUMN model chipset VARCHAR(256);
